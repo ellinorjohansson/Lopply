b12890c6d35c398c131dca39b6a21825bba1441d
Ellinor Johansson <ellinor.johansson@medieinstitutet.se>
2025-12-26 14:01:51 +0100

    💄(style): added style to footer

--- src/common/components/footer/Footer.tsx
+++ src/common/components/footer/Footer.tsx
@@ -1,21 +1,29 @@
+"use client";
+
 import { useTranslation } from "@/common/hooks/useTranslation";
 
 const Footer = () => {
   const g = useTranslation("general");
   const f = useTranslation("footer");
+
   return (
-    <>
-      <footer className="border-t border-secondaryaccent">
+    <footer className="border-t border-secondaryaccent flex flex-col sm:flex-row justify-between items-center sm:items-center text-center sm:text-left gap-y-12 py-6 px-45">
+      {/* Left Column */}
+      <div className="flex flex-col space-y-1 text-center sm:text-left">
         <h2 className="text-3xl">{g("lopply")}</h2>
         <p className="w-40 text-xs">{f("text.discover_and_concuer")}</p>
-        <span className="text-primaryaccent font-sans">
-          {f("scroll_to_top")}
-        </span>
-        <span className="material-symbols-outlined text-primaryaccent">
-          arrow_upward
-        </span>
-      </footer>
-    </>
+        <p className="text-xs mt-2">{f("text.all_rights")}</p>
+      </div>
+
+      {/* Right Column */}
+      <button
+        onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+        className="flex items-center gap-1 cursor-pointer text-primaryaccent mx-auto sm:mx-0"
+      >
+        <span className="text-sm">{f("scroll_to_top")}</span>
+        <span className="material-symbols-outlined text-2xl">arrow_upward</span>
+      </button>
+    </footer>
   );
 };
 
