3219ba497a05771a4ff8c0a54f4377a7fcfa39b3
Ellinor Johansson <ellinor.asaka@gmail.com>
2025-12-29 15:05:18 +0100

    ✨(feature): added succeed toaster component

---
+++ src/common/components/toasters/SuccedToaster.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { useEffect } from "react";
+
+export interface SuccedToasterProps {
+	headerMessage: string;
+	text: string;
+	onClose: () => void;
+}
+
+const SuccedToaster = ({ headerMessage, text, onClose }: SuccedToasterProps) => {
+	useEffect(() => {
+		const timer = setTimeout(() => {
+			onClose();
+		}, 3500);
+
+		return () => clearTimeout(timer);
+	}, [onClose]);
+
+	return (
+		<div
+			className="
+                fixed bottom-10 right-20 bg-secondary text-secondaryaccent shadow-lg border border-secondaryaccent p-4 animate-slide-up rounded-2xl"
+		>
+			<h3 className="font-bold text-sm">{headerMessage}</h3>
+			<p className="text-xs">{text}</p>
+		</div>
+	);
+};
+
+export default SuccedToaster;
