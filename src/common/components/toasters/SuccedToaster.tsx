13e5e43b5ab032817d27b8b7f3a545c1fb9a6d0c
Ellinor Johansson <ellinor.asaka@gmail.com>
2025-12-29 15:20:50 +0100

    💄(style): fixed style on suceed toaster so the text wraps if it's long

--- src/common/components/toasters/SuccedToaster.tsx
+++ src/common/components/toasters/SuccedToaster.tsx
@@ -20,7 +20,8 @@ const SuccedToaster = ({ headerMessage, text, onClose }: SuccedToasterProps) => 
 	return (
 		<div
 			className="
-                fixed bottom-10 right-20 bg-secondary text-secondaryaccent shadow-lg border border-secondaryaccent p-4 animate-slide-up rounded-2xl"
+        fixed bottom-10 right-20 bg-secondary text-secondaryaccent shadow-lg border border-secondaryaccent p-4 animate-slide-up rounded-2xl max-w-xs sm:max-w-sm md:max-w-md
+				wrap-break-words"
 		>
 			<h3 className="font-bold text-sm">{headerMessage}</h3>
 			<p className="text-xs">{text}</p>
