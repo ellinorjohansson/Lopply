da671f6092d7298e1538215c57474f6979899408
Ellinor Johansson <ellinor.asaka@gmail.com>
2025-12-30 20:21:00 +0100

    ✨(feature): added login and signup form

--- src/app/user/login/page.tsx
+++ src/app/user/login/page.tsx
@@ -1,12 +1,54 @@
-import Link from "next/link";
+"use client";
+import PrimaryButton from "@/common/components/buttons/PrimaryButton";
+import InputField from "@/common/components/inputField/InputField";
+import { useTranslation } from "@/common/hooks/useTranslation";
+import { useState } from "react";
 
 const UserLogIn = () => {
+    const a = useTranslation("authentication");
+    const v = useTranslation("validation")
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const [errors, setErrors] = useState<{ email?: string; password?: string }>({});
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+
+        const newErrors: { email?: string; password?: string } = {};
+
+        if (!email) newErrors.email = v("empty_field");
+        if (!password) newErrors.password = v("empty_field");
+
+        setErrors(newErrors);
+
+        if (Object.keys(newErrors).length === 0) {
+            console.log("Temporary: send form", { email, password });
+        }
+    };
+
     return (
         <>
-            <h3 className="border">Userlogin</h3>
-            <Link href="/user/signup"><button>klick</button></Link>
+            <form onSubmit={handleSubmit} className="flex flex-col gap-6">
+                <InputField
+                    label={a("email")}
+                    size="medium"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    error={errors.email}
+                />
+                <InputField
+                    label={a("password")}
+                    size="medium"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    error={errors.password}
+                />
+                <PrimaryButton text={a("user.login")} size="large" />
+            </form>
         </>
-    )
-}
+    );
+};
 
 export default UserLogIn;
--- src/app/user/signup/page.tsx
+++ src/app/user/signup/page.tsx
@@ -1,7 +1,58 @@
+"use client";
+import PrimaryButton from "@/common/components/buttons/PrimaryButton";
+import InputField from "@/common/components/inputField/InputField";
+import { useTranslation } from "@/common/hooks/useTranslation";
+import { useState } from "react";
+
 const UserSignUp = () => {
+    const a = useTranslation("authentication");
+    const v = useTranslation("validation")
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const [errors, setErrors] = useState<{ email?: string; password?: string }>({});
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+
+        const newErrors: { email?: string; password?: string } = {};
+
+        if (!email) newErrors.email = v("empty_field");
+        if (!password) newErrors.password = v("empty_field");
+
+        setErrors(newErrors);
+
+        if (Object.keys(newErrors).length === 0) {
+            console.log("Temporary: send form", { email, password });
+        }
+    };
+
     return (
-        <></>
-    )
-}
+        <>
+            <form onSubmit={handleSubmit} className="flex flex-col gap-6">
+                <InputField
+                    label={a("name")}
+                    size="medium"
+                />
+                <InputField
+                    label={a("email")}
+                    size="medium"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    error={errors.email}
+                />
+                <InputField
+                    label={a("password")}
+                    size="medium"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    error={errors.password}
+                />
+                <PrimaryButton text={a("user.signup")} size="large" />
+            </form>
+        </>
+    );
+};
 
 export default UserSignUp;
