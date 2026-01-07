764b553e3065354b69a9331dc109fa44221a7c0d
Ellinor Johansson <ellinor.asaka@gmail.com>
2026-01-07 13:46:00 +0100

    ✨(feature): added a confirm modal that popup when cancel is pressed

--- src/app/add-race/page.tsx
+++ src/app/add-race/page.tsx
@@ -7,11 +7,13 @@ import InputField from "@/common/components/input/inputField/InputField";
 import { useTranslation } from "@/common/hooks/useTranslation";
 import { useState } from "react";
 import TextArea from "@/common/components/input/TextArea";
+import ConfirmModal from "@/common/components/comfirmModal/ConfirmModal";
 
 const AddRace = () => {
   const b = useTranslation("buttons");
   const a = useTranslation("add_race_page");
   const v = useTranslation("validation");
+  const c = useTranslation("confirm");
 
   const [categoryTerrain, setCategoryTerrain] = useState("");
   const [submitError, setSubmitError] = useState<string | null>(null);
@@ -99,148 +101,165 @@ const AddRace = () => {
     }
   };
 
+  const [cancelPopupOpen, setCancelPopupOpen] = useState(false);
+
   const handleCancel = () => {
-    const confirmed = confirm("Är du säker på att du vill rensa formuläret?");
-    if (confirmed) {
-      setTitle("");
-      setLocations("");
-      setDistance("");
-      setDate("");
-      setCategoryDifficulty("");
-      setCategoryTerrain("");
-      setImageUrl("");
-      setDescription("");
-      setRacePageUrl("");
-      setErrors({});
-    }
+    setCancelPopupOpen(true);
+  };
+
+  const confirmCancel = () => {
+    setTitle("");
+    setLocations("");
+    setDistance("");
+    setDate("");
+    setCategoryDifficulty("");
+    setCategoryTerrain("");
+    setImageUrl("");
+    setDescription("");
+    setRacePageUrl("");
+    setErrors({});
+    setCancelPopupOpen(false);
+  };
+
+  const closeCancelPopup = () => {
+    setCancelPopupOpen(false);
   };
 
   return (
-    <section className="flex items-center justify-center min-h-screen p-4 m-2 md:m-20">
-      <div className="bg-secondary border border-secondaryaccent rounded-3xl p-15 flex flex-col gap-6 max-w-5xl w-full">
-        <div className="flex flex-col gap-1 mb-2">
-          <h3 className="text-2xl font-medium">{a("add_new_race")}</h3>
-          <span className="text-secondaryaccent text-base">{a("submit_a_race")}</span>
+    <>
+      <section className="flex items-center justify-center min-h-screen p-4 m-2 md:m-20">
+        <div className="bg-secondary border border-secondaryaccent rounded-3xl p-15 flex flex-col gap-6 max-w-5xl w-full">
+          <div className="flex flex-col gap-1 mb-2">
+            <h3 className="text-2xl font-medium">{a("add_new_race")}</h3>
+            <span className="text-secondaryaccent text-base">{a("submit_a_race")}</span>
+          </div>
+          <form onSubmit={handleSubmit} className="grid grid-cols-1 md:grid-cols-2 gap-6">
+            <div className="col-span-2 md:col-span-1">
+              <InputField
+                label={a("race_title")}
+                size="medium"
+                type="text"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                error={errors.title}
+              />
+            </div>
+
+            <div className="col-span-2 md:col-span-1">
+              <InputField
+                label={a("locations")}
+                size="medium"
+                type="text"
+                value={locations}
+                onChange={(e) => setLocations(e.target.value)}
+                error={errors.locations}
+                helpButton={<HelperButton infoText="" />}
+              />
+            </div>
+
+            <div className="col-span-2 md:col-span-1">
+              <InputField
+                label={a("distance")}
+                size="medium"
+                type="number"
+                value={distance}
+                onChange={(e) => setDistance(e.target.value)}
+                error={errors.distance}
+                helpButton={<HelperButton infoText="" />}
+              />
+            </div>
+
+            <div className="col-span-2 md:col-span-1">
+              <InputField
+                label={a("date")}
+                size="medium"
+                type="date"
+                value={date}
+                onChange={(e) => setDate(e.target.value)}
+                error={errors.date}
+              />
+            </div>
+
+            <div className="col-span-2 md:col-span-1">
+              <DropdownField
+                label={a("difficulty")}
+                value={categoryDifficulty}
+                onChange={(e) => setCategoryDifficulty(e.target.value)}
+                error={errors.categoryDifficulty}
+                options={[
+                  { value: a("difficulty_options.easy"), label: a("difficulty_options.easy") },
+                  { value: a("difficulty_options.medium"), label: a("difficulty_options.medium") },
+                  { value: a("difficulty_options.hard"), label: a("difficulty_options.hard") },
+                ]}
+              />
+            </div>
+
+            <div className="col-span-2 md:col-span-1">
+              <DropdownField
+                label={a("terrain")}
+                value={categoryTerrain}
+                onChange={(e) => setCategoryTerrain(e.target.value)}
+                error={errors.categoryTerrain}
+                options={[
+                  { value: a("terrain_options.urban"), label: a("terrain_options.urban") },
+                  { value: a("terrain_options.costal"), label: a("terrain_options.costal") },
+                  { value: a("terrain_options.mountain"), label: a("terrain_options.mountain") },
+                  { value: a("terrain_options.desert"), label: a("terrain_options.desert") },
+                  { value: a("terrain_options.forest"), label: a("terrain_options.forest") },
+                ]}
+              />
+            </div>
+
+            <div className="col-span-2">
+              <InputField
+                label={a("image_url")}
+                size="large"
+                type="text"
+                value={imageUrl}
+                onChange={(e) => setImageUrl(e.target.value)}
+                error={errors.imageUrl}
+                helpButton={<HelperButton infoText="" />}
+              />
+            </div>
+
+            <div className="col-span-2">
+              <TextArea
+                label={a("description")}
+                value={description}
+                onChange={setDescription}
+                helpButton={<HelperButton infoText="" />}
+              />
+            </div>
+
+            <div className="col-span-2">
+              <InputField
+                label={a("url_race_page")}
+                size="large"
+                type="text"
+                value={racePageUrl}
+                onChange={(e) => setRacePageUrl(e.target.value)}
+                error={errors.racePageUrl}
+              />
+            </div>
+
+            <div className="col-span-2 flex flex-row gap-3 mt-8">
+              <PrimaryButton text={b("add_race")} size="large" />
+              <SecondaryButton text={b("cancel")} size="small" onClick={handleCancel} />
+            </div>
+          </form>
+          {submitSuccess && <div className="text-green-500 text-center">Race added successfully!</div>}
+          {submitError && <div className="text-red-500 text-center">{submitError}</div>}
+          <span className="text-sm text-secondaryaccent">{v("required_field")}</span>
         </div>
-        <form onSubmit={handleSubmit} className="grid grid-cols-1 md:grid-cols-2 gap-6">
-          <div className="col-span-2 md:col-span-1">
-            <InputField
-              label={a("race_title")}
-              size="medium"
-              type="text"
-              value={title}
-              onChange={(e) => setTitle(e.target.value)}
-              error={errors.title}
-            />
-          </div>
-
-          <div className="col-span-2 md:col-span-1">
-            <InputField
-              label={a("locations")}
-              size="medium"
-              type="text"
-              value={locations}
-              onChange={(e) => setLocations(e.target.value)}
-              error={errors.locations}
-              helpButton={<HelperButton infoText="" />}
-            />
-          </div>
-
-          <div className="col-span-2 md:col-span-1">
-            <InputField
-              label={a("distance")}
-              size="medium"
-              type="number"
-              value={distance}
-              onChange={(e) => setDistance(e.target.value)}
-              error={errors.distance}
-              helpButton={<HelperButton infoText="" />}
-            />
-          </div>
-
-          <div className="col-span-2 md:col-span-1">
-            <InputField
-              label={a("date")}
-              size="medium"
-              type="date"
-              value={date}
-              onChange={(e) => setDate(e.target.value)}
-              error={errors.date}
-            />
-          </div>
-
-          <div className="col-span-2 md:col-span-1">
-            <DropdownField
-              label={a("difficulty")}
-              value={categoryDifficulty}
-              onChange={(e) => setCategoryDifficulty(e.target.value)}
-              error={errors.categoryDifficulty}
-              options={[
-                { value: a("difficulty_options.easy"), label: a("difficulty_options.easy") },
-                { value: a("difficulty_options.medium"), label: a("difficulty_options.medium") },
-                { value: a("difficulty_options.hard"), label: a("difficulty_options.hard") },
-              ]}
-            />
-          </div>
-
-          <div className="col-span-2 md:col-span-1">
-            <DropdownField
-              label={a("terrain")}
-              value={categoryTerrain}
-              onChange={(e) => setCategoryTerrain(e.target.value)}
-              error={errors.categoryTerrain}
-              options={[
-                { value: a("terrain_options.urban"), label: a("terrain_options.urban") },
-                { value: a("terrain_options.costal"), label: a("terrain_options.costal") },
-                { value: a("terrain_options.mountain"), label: a("terrain_options.mountain") },
-                { value: a("terrain_options.desert"), label: a("terrain_options.desert") },
-                { value: a("terrain_options.forest"), label: a("terrain_options.forest") },
-              ]}
-            />
-          </div>
-
-          <div className="col-span-2">
-            <InputField
-              label={a("image_url")}
-              size="large"
-              type="text"
-              value={imageUrl}
-              onChange={(e) => setImageUrl(e.target.value)}
-              error={errors.imageUrl}
-              helpButton={<HelperButton infoText="" />}
-            />
-          </div>
-
-          <div className="col-span-2">
-            <TextArea
-              label={a("description")}
-              value={description}
-              onChange={setDescription}
-              helpButton={<HelperButton infoText="" />}
-            />
-          </div>
-
-          <div className="col-span-2">
-            <InputField
-              label={a("url_race_page")}
-              size="large"
-              type="text"
-              value={racePageUrl}
-              onChange={(e) => setRacePageUrl(e.target.value)}
-              error={errors.racePageUrl}
-            />
-          </div>
-
-          <div className="col-span-2 flex flex-row gap-3 mt-8">
-            <PrimaryButton text={b("add_race")} size="large" />
-            <SecondaryButton text={b("cancel")} size="small" onClick={handleCancel} />
-          </div>
-        </form>
-        {submitSuccess && <div className="text-green-500 text-center">Race added successfully!</div>}
-        {submitError && <div className="text-red-500 text-center">{submitError}</div>}
-        <span className="text-sm text-secondaryaccent">{v("required_field")}</span>
-      </div>
-    </section>
+      </section>
+      <ConfirmModal
+        open={cancelPopupOpen}
+        title={c("clear_form")}
+        message={c("are_you_sure")}
+        onConfirm={confirmCancel}
+        onCancel={closeCancelPopup}
+      />
+    </>
   );
 };
 
