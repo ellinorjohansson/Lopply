8918b132f2f9931cf5bef5ca1dac166a671d8078
Ellinor Johansson <ellinor.asaka@gmail.com>
2026-01-12 14:32:06 +0100

    🐛(bug): fixed so it doesn't reload

--- src/common/components/buttons/FilterButton.tsx
+++ src/common/components/buttons/FilterButton.tsx
@@ -10,13 +10,14 @@ export interface FilterButtonProps {
 const FilterButton = ({ text, icon, isActive = false, onClick }: FilterButtonProps) => {
   return (
     <button
+      type="button"
       onClick={onClick}
       className={`py-2 px-4 font-sans flex items-center cursor-pointer rounded-3xl transition min-h-44px ${isActive
         ? "bg-primaryaccent text-secondaryaccent"
         : "bg-secondary border border-secondaryaccent text-secondaryaccent hover:bg-primaryaccent/10"
         }`}
     >
-      <span className="material-symbols-outlined mr-2 text-sm">
+      <span className="material-symbols-outlined mr-2 text-sm " aria-hidden="true">
         {icon}
       </span>
       {text}
