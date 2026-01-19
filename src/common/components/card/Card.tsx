fdb3fc381d124609b4b934ece17d43a17e768dfe
Ellinor Johansson <ellinor.asaka@gmail.com>
2026-01-19 10:52:36 +0100

    💄(style): fixed so the race cards in bucketlist has the same heigh

--- src/common/components/card/Card.tsx
+++ src/common/components/card/Card.tsx
@@ -351,7 +351,7 @@ const Card = ({
 				href={raceUrl}
 				target="_blank"
 				rel="noopener noreferrer"
-				className="block"
+				className="block h-full"
 			>
 				<article
 					className="rounded-3xl overflow-hidden bg-secondary text-secondaryaccent w-80 shadow-lg cursor-pointer hover:scale-103 transition flex flex-col h-full"
